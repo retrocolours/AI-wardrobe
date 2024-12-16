53116d3c2cdf8d8b5d71e6a7a44a3873889d475f
Tatiana Privezentseva <severny.island@gmail.com>
2024-12-16 12:03:18 -0800

    set up project

---
+++ Models/ItemType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AI_Wardrobe.Models;
+
+public partial class ItemType
+{
+    public int Itemtypeid { get; set; }
+
+    public string? Itemtypedescription { get; set; }
+
+    public virtual ICollection<Item> Items { get; set; } = new List<Item>();
+}
---
+++ Models/OrderDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AI_Wardrobe.Models;
+
+public partial class OrderDetail
+{
+    public int Orderdetailsid { get; set; }
+
+    public int? Fkitemid { get; set; }
+
+    public DateOnly? Quantity { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public int? Fkorderid { get; set; }
+
+    public virtual Item? Fkitem { get; set; }
+
+    public virtual Order? Fkorder { get; set; }
+}
