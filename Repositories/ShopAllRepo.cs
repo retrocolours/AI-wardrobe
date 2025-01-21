e425b1de855e5a587c9e44a1c511a89928b26e22
Tatiana Privezentseva <severny.island@gmail.com>
2025-01-21 13:34:51 -0800

    attempt on making all product page

--- Repositories/ShopAllRepo.cs
+++ Repositories/ShopAllRepo.cs
@@ -3,6 +3,7 @@
 using AI_Wardrobe.Data;
 using AI_Wardrobe.ViewModels;
 using AI_Wardrobe.Models;
+using System.Collections.Generic;
 
 namespace AI_Wardrobe.Repositories
 {
@@ -18,17 +19,18 @@ namespace AI_Wardrobe.Repositories
         // Get specific product in the database.
         public ShopAllVM? GetProduct(int id)
         {
-            var item = _context.Items?
-                .Select(p => new ShopAllVM
+            var item = _context.Items
+                .Where(i => i.Itemid == id)
+                .Select(i=> new ShopAllVM
                 {
-                    ID = p.ID,
-                    ProductName = p.ProductName,
-                    Description = p.Description,
-                    Price = p.Price,
-                    Currency = p.Currency,
-                    Image = p.Image
-                })
-                .FirstOrDefault(p => p.ID == id);
+                    ID = i.Itemid,
+                    Type = i.Itemtype!,
+                    Description = i.Itemdescription,
+                    Price = i.Itemprice,
+                    Image = i.Imageurl
+                })                            
+                .FirstOrDefault();
+;
 
             return item;
         }
@@ -37,17 +39,16 @@ namespace AI_Wardrobe.Repositories
         public IEnumerable<ShopAllVM> GetAllProducts()
         {
             var items = _context.Items?
-                .Select(p => new ShopAllVM
+                .Select(i => new ShopAllVM
                 {
-                    ID = p.ID,
-                    ProductName = p.ProductName,
-                    Description = p.Description,
-                    Price = p.Price,
-                    Currency = p.Currency,
-                    Image = p.Image
+                    ID = i.Itemid,
+                   Type = i.Itemtype!,
+                    Description = i.Itemdescription,
+                    Price = i.Itemprice,
+                    Image = i.Imageurl
                 }) ?? Enumerable.Empty<ShopAllVM>();
 
-            return items;
+            return items!;
         }
     }
 }
--- ViewModels/ShopAllVM.cs
+++ ViewModels/ShopAllVM.cs
@@ -6,17 +6,17 @@ namespace AI_Wardrobe.ViewModels
     {
         public int ID { get; set; }
 
-        [Display(Name = "Product Name")]
-        public string? ProductName { get; set; }
+        // [Display(Name = "Product Name")]
+        // public string? ProductName { get; set; }
+
+    [Display(Name = "Product Name")]
+        public string Type { get; set; }
 
         [Display(Name = "Description")]
         public string? Description { get; set; }
 
         [Display(Name = "Price")]
-        public string? Price { get; set; }
-
-        [Display(Name = "Currency")]
-        public string? Currency { get; set; }
+        public decimal? Price { get; set; }
 
         [Display(Name = "Image")]
         public string? Image { get; set; }
