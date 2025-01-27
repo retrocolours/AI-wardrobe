ef17c01c42e07be0383aa10503ecb3a472674fed
JBB0807 <104856796+JBB0807@users.noreply.github.com>
2025-01-27 10:43:11 -0800

    added RegisteredUserVM UserController and others

---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using AI_Wardrobe.Repositories;
+
+namespace AI_Wardrobe.Controllers
+{
+    public class UserController
+    {
+
+        private readonly UserRepo _userRepo;
+
+        public UserController(UserRepo userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+
+
+    }
+}
