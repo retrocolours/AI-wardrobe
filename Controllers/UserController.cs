0e6c98e7abba00883e945a0e55b3e185bbbe49d1
FoyBuors17 <foybuors17@gmail.com>
2025-01-29 11:52:01 -0800

    userPage

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,18 +1,27 @@
-﻿using AI_Wardrobe.Repositories;
+﻿using AIWardrobe.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace AI_Wardrobe.Controllers
+namespace AIWardrobe.Controllers
 {
-    public class UserController
+    public class UserController : Controller
     {
-
-        private readonly UserRepo _userRepo;
-
-        public UserController(UserRepo userRepo)
+        public IActionResult MyProfile()
         {
-            _userRepo = userRepo;
+            var user = new User
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Address = "555 Seymour St",
+                Apartment = "",
+                City = "Vancouver",
+                Province = "BC",
+                PostalCode = "V6B 3H6",
+                Phone = "",
+                Email = "email@janesfakedomain.net"
+            };
+
+            return Json(user);
         }
-
-
-
     }
 }
