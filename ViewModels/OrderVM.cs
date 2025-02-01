8fae6dd15b129b2be0645390dd440742e36e3bb9
JBB0807 <104856796+JBB0807@users.noreply.github.com>
2025-01-31 16:28:33 -0800

    merge with my profile

---
+++ ViewModels/OrderVM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AI_Wardrobe.ViewModels
+{
+    public class OrderVM
+    {
+        [Display(Name = "Order ID")]
+        public int Id { get; set; }
+
+        [Display(Name = "Order Date")]
+        public DateOnly? Date { get; set; }
+
+        [Display(Name = "Order Status")]
+        public string? Status { get; set; }
+
+        [Display(Name = "Ordered By")]
+        public string? OrderedBy { get; set; }
+
+        [Display(Name = "Delievery Address")]
+        public string? DeliverAddress { get; set; }
+       
+        public TransactionVM? TransactionVM { get; set; }
+
+        public IEnumerable<OrderDetailVM>? OrderDetailVMs;
+    }
+}
