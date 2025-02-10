f65cf08d5a570dcea2a37f6a42dbb9e1eb914ca1
Tatiana Privezentseva <severny.island@gmail.com>
2025-02-10 14:28:02 -0800

    test

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -4,26 +4,25 @@ namespace AI_Wardrobe.Models
 {
     public class Transaction
     {
-        public int Transactionid { get; set; } // Unique ID for transaction
+        public int Transactionid { get; set; } 
 
-        public string? PayPalTransactionId { get; set; } // PayPal Transaction ID
+        public string? PayPalTransactionId { get; set; } 
 
-        public decimal? Totalamount { get; set; } // Transaction Amount
+        public decimal? Totalamount { get; set; } 
 
-        public DateTime? Transactiondate { get; set; }  // Date of transaction
+        public DateTime? Transactiondate { get; set; }  
 
-        public string? Transactionstatus { get; set; } // Completed, Failed, etc.
+        public string? Transactionstatus { get; set; } 
 
-        public string? PayerName { get; set; } // Payer's Name from PayPal
+        public string? PayerName { get; set; } 
 
-        public string? PayerEmail { get; set; } // Payer's Email from PayPal
+        public string? PayerEmail { get; set; } 
+        public string? Currency { get; set; } 
 
-        public string? Currency { get; set; } // Currency of the transaction (USD, CAD, etc.)
+        public string? PaymentMethod { get; set; } 
 
-        public string? PaymentMethod { get; set; } // PayPal, Credit Card, etc.
+        public int? Fkorderid { get; set; } 
 
-        public int? Fkorderid { get; set; } // Foreign key to Order table
-
-        public virtual Order? Fkorder { get; set; } // Relationship with Order
+        public virtual Order? Fkorder { get; set; } 
     }
 }
